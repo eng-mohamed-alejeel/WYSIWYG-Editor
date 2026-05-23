6969e9179a90cc89b853ff0d02665c47656ce16e
Mohamed Alejeel <mohamedalejeel1991@gmail.com>
2026-05-23 23:24:34 +0300

    Remove dist files from tracking and add to .gitignore

--- packages/shared/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * WYSIWYG Editor Shared Package
- *
- * This is the main entry point for the shared package.
- * It exports all constants and utility functions.
- */
-export * from './constants';
-export * from './utils';
-//# sourceMappingURL=index.d.ts.map
